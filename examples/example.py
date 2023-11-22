277b1f0af520423c4b643386dec2e53919cf3ef0
Thierry Moudiki <thierry.moudiki@gmail.com>
2023-11-22 09:14:12 +0000

    use a csv available online (not just on Desktop)

--- examples/example.py
+++ examples/example.py
@@ -1,38 +1,39 @@
 import forecastingapi as fapi
+import pandas as pd 
 
 if __name__ == "__main__":
     
     ## 1 - create an account (once)
-    res_create_account = fapi.create_account(username="user@example.com", 
-                                             password="pwd") # choose a better password
-    print(res_create_account)
+    #res_create_account = fapi.create_account(username="user20231122@example.com", 
+    #                                         password="pwd") # choose a better password
+    #print(res_create_account)
 
-    ## 2 - get a token 
-    token = fapi.get_token(username = "user@example.com",
+    # 2 - get a token 
+    token = fapi.get_token(username = "user20231122@example.com",
                                    password = "pwd")
     print(token)
 
     ## 3 - get forecast with prediction interval
-    path_to_file = '/Users/t/Documents/datasets/time_series/univariate/nile.csv' # (examples:
-#' https://github.com/Techtonique/datasets/tree/main/time_series/univariate)
+    path_to_file = 'https://raw.githubusercontent.com/Techtonique/datasets/main/time_series/univariate/AirPassengers.csv'
+    #path_to_file = './datasets/AirPassengers.csv'
      
     res_get_forecast = fapi.get_forecast(file=path_to_file, 
     token=token)
 
-    print(res_get_forecast)
+    print(pd.DataFrame(res_get_forecast))
 
     res_get_forecast2 = fapi.get_forecast(file=path_to_file, 
     token=token, start_training = 2, n_training = 7, h = 4, level = 90)
 
-    print(res_get_forecast2)
+    print(pd.DataFrame(res_get_forecast2))
 
     res_get_forecast3 = fapi.get_forecast(file=path_to_file, 
     token=token, date_formatting="ms",
     start_training = 2, n_training = 7, h = 4, level = 90)
 
-    print(res_get_forecast3)
+    print(pd.DataFrame(res_get_forecast3))
 
     res_get_forecast4 = fapi.get_forecast(file=path_to_file, 
     token=token, method = "prophet")
 
-    print(res_get_forecast4)
+    print(pd.DataFrame(res_get_forecast4))
--- forecastingapi/__init__.py
+++ forecastingapi/__init__.py
@@ -1,8 +1,8 @@
 """Top-level package for forecastingAPI."""
 
 __author__ = """T. Moudiki"""
-__email__ = 'thierry.moudiki@gmail.com'
-__version__ = '0.1.0'
+__email__ = "thierry.moudiki@gmail.com"
+__version__ = "0.2.0"
 
 from .forecastingapi import create_account, get_token, get_forecast
 
